4cefeca4e41853896ba3b62e85c5edded419334c
Anubis Tyrant <adwinpaulji@gmail.com>
2024-08-04 21:18:51 +0530

    Code Cleanup

--- obj/Release/net8.0-windows7.0/Spaceshootergame-dotnetassignment.AssemblyInfo.cs
+++ obj/Release/net8.0-windows7.0/Spaceshootergame-dotnetassignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spaceshootergame-dotnetassignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1846aa61256596fbf9c3db16dc6ce1aef4f1e01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a29e446ace408b88628b9434511fb5192f9f8b03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spaceshootergame-dotnetassignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spaceshootergame-dotnetassignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
